1961aae9e6ff088c768dbc3a59af5abca07def04
Threem4y <Andreyka0193@mail.ru>
2023-05-26 09:21:41 +0300

    first comkit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LibramonyClass;
+using Newtonsoft.Json;
+
+namespace Libramony
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CasualTbx_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private static void DeserelializeButton_Click<T>(object sender, RoutedEventArgs e)
+        {
+           
+
+            
+        }
+
+        private static void SerelializeButton_Click(object sender, RoutedEventArgs e, List<string> Jesons)
+        {
+
+        }
+
+        private void LightButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DarkButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EngButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RusButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
